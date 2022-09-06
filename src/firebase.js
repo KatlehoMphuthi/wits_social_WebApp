6b21d0e2d868887f2ac794afe1f46ee7fdc44ac7
Katleho <katli.mphuthi@gmail.com>
2022-09-06 10:55:17 +0200

    Testing Reset password

--- src/firebase.js
+++ src/firebase.js
@@ -104,19 +104,22 @@ function getCurrentUser(){
 
 // function to reset the password 
 export function resetPass(email){
-  sendPasswordResetEmail(auth, email)
-  .then(() => {
-    // Password reset email sent!
-    console.log("success!");
-    return "success";
-    
-  })
-  .catch((error) => {
-    const errorMessage = error.message;
-    console.log(errorMessage);
-    return errorMessage;
+  let errorMessage; 
+  try{
+    const res  = sendPasswordResetEmail(auth, email)
+    console.log("success")
+    errorMessage = "success";
+  }catch(error) {
+    errorMessage = error.message;
+    if(errorMessage === "Firebase: Error (auth/user-not-found)"){
+      alert("You are not a registered user. Please proceed to the sign up page.");
+    }else{
+      alert("An error has occured!");
+    }
     // ..
-  });
+  };
+
+  return errorMessage;
 }
 
 // get the user information
