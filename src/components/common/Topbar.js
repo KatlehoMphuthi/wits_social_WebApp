0d565198593b6971fc9a5bfa111fe23163a8fa72
Kgotso <Kgotsokg12@gmail.com>
2022-09-20 07:50:51 +0200

    made username to show on the top bar

--- src/components/common/Topbar.js
+++ src/components/common/Topbar.js
@@ -1,23 +1,45 @@
 import "./topbar.css";
+import {getUserinfo, getUserObject, getCurrentUser, database, readData} from "../../firebase"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
-import { getUserObject, logout } from "../../firebase";
 import { AuthContext } from "../../AuthProvider";
-import {getUserinfo, getCurrentUser} from "../../firebase"
+import {onValue,ref } from "firebase/database";
 
 export default function Topbar() {
-  let user;
 const [fname, setfname] = useState("")
 
- 
+useEffect(()=>{
+  setfname(readData())
+}, [fname])
 
- // console.log(getCurrentUser())
- console.log(user.firstname, user.lastName)
-
-  return (
+return fname === "" ? 
+  (
+      <div className="topbarContainer">
+        <div className="topbarLeft">
+          <span className="logo"><img src="/svg/WS_Logo.svg" alt="" width={65}/></span>
+        </div>
+        <div className="topbarCenter">
+          <div className="searchbar">
+              <FontAwesomeIcon icon={faMagnifyingGlass} className="searchbar-icon"/>
+            <input
+            
+              placeholder="Search for friend, post or video coming Soon"
+              className="searchInput"
+            />
+          </div>
+        </div>
+        <div className="topbarRight">
+          <div className="topbarLinks">
+          <span> <p>Loading ... </p></span>
+          </div>
+        </div>
+      </div>
+  )
+  :
+  (
     <div className="topbarContainer">
       <div className="topbarLeft">
         <span className="logo"><img src="/svg/WS_Logo.svg" alt="" width={65}/></span>
@@ -34,10 +56,10 @@ const [fname, setfname] = useState("")
       </div>
       <div className="topbarRight">
         <div className="topbarLinks">
-        <span> <p>{user.firstname}</p></span>
+        <span><p>{fname}</p></span>
         </div>
-        
       </div>
     </div>
-  );
+  )
+ ;
 }
