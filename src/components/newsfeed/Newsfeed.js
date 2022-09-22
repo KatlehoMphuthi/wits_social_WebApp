d8515c43b2f78139a72a5b0e360e138e52b34244
Katleho <katli.mphuthi@gmail.com>
2022-09-22 14:09:29 +0200

    Update Newsfeed.js

--- src/components/newsfeed/Newsfeed.js
+++ src/components/newsfeed/Newsfeed.js
@@ -11,7 +11,37 @@ import {onValue,ref} from 'firebase/database';
 
 function Newsfeed(){
   const {currentUser} = useContext(AuthContext);
-  const [posts,setPost] = useState([]);
+  const [posts,setPost] = useState([
+    {
+    username : "Michael",
+     caption : "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout",
+     imgUrl : "https://i.ytimg.com/vi/zeO1yrVeC0U/maxresdefault.jpg ",
+     name:"@MichaelM"
+    },
+    {
+      username:"BMW Motors",
+       caption:" Carry out a random act of kindness, with no expectation of reward, safe in the knowledge that one day someone might do the same for you",
+      imgUrl:"https://source.unsplash.com/random/100*200",
+      name:"@OfficialBMWMotors"
+     
+    },
+    {
+      username:"Boera",
+       caption:"", 
+       imgUrl:"https://source.unsplash.com/random/100*210",
+       name:"@Human"
+    }
+
+    ,
+    
+    {
+      username:"Lebohang",
+       caption:"", 
+       imgUrl:"https://source.unsplash.com/random/100*205",
+       name:"@Human"
+    }
+    
+  ]);
 
   useEffect(() =>{
     if(currentUser){
