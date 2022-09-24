67d3d3aaa1bcfd5bf591f1b07e92255180aa4f2d
Katleho <katli.mphuthi@gmail.com>
2022-09-24 18:11:37 +0200

    Testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { AuthProvider} from "./AuthProvider";
+import { Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from "react-alert-template-mui";
+import {BrowserRouter as Router} from 'react-router-dom';
 
 test('renders home page', () => {
-  render(<App />);
+  render(
+    <AlertProvider template={AlertTemplate}>
+    <AuthProvider>
+    
+        <App />
+   
+  </AuthProvider>
+  </AlertProvider>);
+
   const linkElement = screen.getAllByRole('button', {
     name: /sign in/i
   })[0];
