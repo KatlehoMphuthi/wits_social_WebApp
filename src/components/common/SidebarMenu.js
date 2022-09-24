b22f6077972b685b240f74da364509ee16f21ff7
Katleho <katli.mphuthi@gmail.com>
2022-09-24 06:56:36 +0200

    Added an about component
    Added an about page

--- src/components/common/SidebarMenu.js
+++ src/components/common/SidebarMenu.js
@@ -23,17 +23,29 @@ function SidebarMenu() {
     
   };
 
+  const goToAbout = () => {
+    if(currentUser){
+      navigate('/about',{replace:true});
+    }
+  }
+
+  const goToHome = () =>{
+    if(currentUser){
+      navigate('/newsfeed',{replace:true});
+    }
+  }
+
 
   return (
     <div className="sidebar-menu">
-    <div className="sidebar-menu__item sidebar-menu__item--active">
+    <div className="sidebar-menu__item sidebar-menu__item--active" onClick={goToHome}>
       <img src="./svg/home.svg" className="sidebar-menu__item-icon" />
       Home
     </div>
 
-    <div className="sidebar-menu__item">
+    <div className="sidebar-menu__item" onClick={goToAbout}>
       <img src="./svg/explore.svg" className="sidebar-menu__item-icon" />
-      Explore
+      About
     </div>
 
     <div className="sidebar-menu__item">
