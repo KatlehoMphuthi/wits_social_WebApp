ba5a8a90889b00d1252934eb0019321dbf692dba
Katleho <64352683+KatlehoMphuthi@users.noreply.github.com>
2022-10-28 15:42:34 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from './components/authentication/Login';
 import Register from './components/authentication/Registration';
